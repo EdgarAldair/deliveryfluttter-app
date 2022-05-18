f4c4b41c5aca180d4fd2f6eae56270be7e192b28
Edgar Saenz <edgarsaenz243@gmail.com>
2022-05-17 22:09:45 -0500

    Login diseño

--- lib/src/login/login_page.dart
+++ lib/src/login/login_page.dart
@@ -1,4 +1,7 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
+import 'package:flutter_delivery/src/utils/my_colors.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -11,24 +14,131 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('data'),
+      body: SizedBox(
+        width: double.infinity,
+        child: Stack(
+          children: [
+            Positioned(top: -80, left: -100, child: _circleLogin()),
+            Positioned(top: 60, left: 25, child: _textLogin()),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                _imageBanner(),
+                _textfieldEmail(),
+                _textfieldPassword(),
+                _botonRegistrar(),
+                _textRegistrar(),
+              ],
+            ),
+          ],
+        ),
       ),
-      body: Stack(
-        children: [
-          Container(
-            width: 150,
-            height: 150,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.all(Radius.circular(100)),
-                color: Colors.red),
-          ),
-          Container(
-            margin: EdgeInsets.only(left: 55, top: 65),
-            child: Text('LOGIN'),
-          )
-        ],
+    );
+  }
+
+  Widget _textLogin() {
+    return Text(
+      'LOGIN',
+      style: TextStyle(
+          color: Colors.white, fontSize: 22, fontWeight: FontWeight.bold),
+    );
+  }
+
+  Widget _textRegistrar() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(
+          'No tienes cuenta?',
+          style: TextStyle(color: MyColors.primaryColor),
+        ),
+        SizedBox(
+          width: 7,
+        ),
+        Text(
+          'Regístrate',
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: MyColors.primaryColor),
+        )
+      ],
+    );
+  }
+
+  Widget _botonRegistrar() {
+    return Container(
+      width: double.infinity,
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 30),
+      child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30)),
+              padding: EdgeInsets.symmetric(vertical: 15)),
+          child: Text('Ingresar')),
+    );
+  }
+
+  Widget _textfieldPassword() {
+    return Container(
+      decoration: BoxDecoration(
+          color: MyColors.primaryOpacityColor,
+          borderRadius: BorderRadius.circular(30)),
+      // ignore: prefer_const_constructors
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+      child: TextField(
+        decoration: InputDecoration(
+            hintText: 'Contraseña',
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.all(15),
+            hintStyle: TextStyle(color: MyColors.primaryColorDark),
+            prefixIcon: Icon(
+              Icons.lock,
+              color: MyColors.primaryColor,
+            )),
       ),
+    );
+  }
+
+  Widget _textfieldEmail() {
+    return Container(
+      decoration: BoxDecoration(
+          color: MyColors.primaryOpacityColor,
+          borderRadius: BorderRadius.circular(30)),
+      // ignore: prefer_const_constructors
+      margin: EdgeInsets.symmetric(horizontal: 50, vertical: 5),
+      child: TextField(
+        decoration: InputDecoration(
+            hintText: 'Correo electrónico',
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.all(15),
+            hintStyle: TextStyle(color: MyColors.primaryColorDark),
+            prefixIcon: Icon(
+              Icons.email,
+              color: MyColors.primaryColor,
+            )),
+      ),
+    );
+  }
+
+  Widget _imageBanner() {
+    return Container(
+      margin: EdgeInsets.only(
+          top: 100, bottom: MediaQuery.of(context).size.height * 0.22),
+      child: Image.asset(
+        'assets/img/delivery.png',
+        width: 200,
+        height: 200,
+      ),
+    );
+  }
+
+  Widget _circleLogin() {
+    return Container(
+      height: 240,
+      width: 230,
+      decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(100),
+          color: MyColors.primaryColor),
     );
   }
 }
